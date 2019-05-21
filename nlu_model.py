fbca3d3503a9f396238950a634916d33d70f71d3
jibinmathew69 <jibin.mathew.69@gmail.com>
2019-05-21 11:22:10 +0530

    Add create_nlu()

--- nlu_model.py
+++ nlu_model.py
@@ -3,3 +3,24 @@ from rasa_nlu.model import Trainer
 from rasa_nlu import config
 
 
+def create_nlu(data_file="data/nlu_data.md",
+               config_file="nlu_config.yml"):
+    '''
+    This function trains NLU model and persists the model
+    :param data_file: data file for NLU
+    :param config_file: config file for NLU
+    :return:
+    '''
+    # loading the nlu training samples
+    training_data = load_data(data_file)
+
+    # trainer to educate our pipeline
+    trainer = Trainer(config.load(config_file))
+
+    # train the model!
+    interpreter = trainer.train(training_data)
+
+    # store it for future use
+    model_directory = trainer.persist("./models/nlu", fixed_model_name="current")
+
+    return interpreter, model_directory
