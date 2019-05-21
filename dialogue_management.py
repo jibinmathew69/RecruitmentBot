89b9c96aafdeb05f51de7492fa5d6b98df457807
jibinmathew69 <jibin.mathew.69@gmail.com>
2019-05-21 13:30:22 +0530

    Refactor to seperate chat

--- dialogue_management.py
+++ dialogue_management.py
@@ -1,34 +1,8 @@
 import rasa_core
 from rasa_core.agent import Agent
-from rasa_core.policies.keras_policy import KerasPolicy
-from rasa_core.policies.memoization import MemoizationPolicy
-from rasa_core.policies.fallback import FallbackPolicy
 from rasa_core.interpreter import RasaNLUInterpreter
 from rasa_core.utils import EndpointConfig
 from rasa_core.run import serve_application
-
-
-def train_dialogue(domain_file='domain.yml',
-                   model_path='./models/dialogue',
-                   training_data_file='./data/stories.md'):
-    """
-    This model trains the dialogue model and persist the model
-    :param domain_file: Domain file
-    :param model_path: Model persistence directory
-    :param training_data_file: Story board file
-    :return: Agent
-    """
-    fallback = FallbackPolicy(fallback_action_name="action_default_fallback",
-                              core_threshold=0.3,
-                              nlu_threshold=0.3)
-
-    agent = Agent(domain_file, policies=[MemoizationPolicy(max_history=3), KerasPolicy(max_history=5, epochs=300, batch_size=50), fallback])
-    data = agent.load_data(training_data_file)
-
-    agent.train(data)
-
-    agent.persist(model_path)
-    return agent
 
 
 def run_recruitment_bot():
@@ -45,5 +19,4 @@ def run_recruitment_bot():
 
 
 if __name__ == '__main__':
-    train_dialogue()
     run_recruitment_bot()
