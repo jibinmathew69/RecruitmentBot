21f51a0988186e1a75eb33b5600199962cb34b44
jibinmathew69 <jibin.mathew.69@gmail.com>
2019-05-21 11:35:02 +0530

    Add actions

---
+++ actions.py
@@ -0,0 +1,37 @@
+from rasa_core_sdk import Action
+from rasa_core_sdk.events import SlotSet
+import random
+
+
+class ActionCheckStatus(Action):
+
+    def name(self):
+        return "action_check_status"
+
+    def run(self, dispatcher, tracker, domain):
+        # return a random status, just a mockup
+        statuses = ["received", "rejected", "interview", "unknown"]
+        status = random.choice(statuses)
+        return [SlotSet("status", status)]
+
+
+class ActionCheckPositions(Action):
+
+    def name(self):
+        return "action_check_positions"
+
+    def run(self, dispatcher, tracker, domain):
+        # return hard-coded open positions, this would normally come from an API
+        positions = {
+            "technical": [
+                "machine learning engineer",
+                "ML product success engineer"
+            ],
+            "business": []
+        }
+        position_type = tracker.get_slot("role_type")
+        if position_type == "any":
+            relevant_positions = positions["technical"] + positions["business"]
+        else:
+            relevant_positions = positions.get(position_type, [])
+        return [SlotSet("positions", relevant_positions)]
